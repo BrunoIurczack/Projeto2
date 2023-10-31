d13973abd6437c691aa5dfef4682ee61dda29a7e
Bruno Felipe <bruno@matheuss-MacBook-Air-2.local>
2023-10-31 09:41:53 -0300

    End of project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  privante
+  
+    def require_logged_in_user
+      unless user_signed_in?
+        flash[:danger] = 'Area restrista. Por favor, realize o login'
+        redirect_to entrar_path
+      end
+    end
+
 end
